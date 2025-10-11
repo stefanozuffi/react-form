578ce4c453e77703baa0da47d5ef8ea2ee1bbcd7
stefanozuffi <stefano.zuffi06@outlook.com>
2025-10-11 11:18:03 +0200

    basic implementation of a download/publish online icon, connected witgh checkbox

--- src/assets/components/AppList.jsx
+++ src/assets/components/AppList.jsx
@@ -1,4 +1,4 @@
-import articles from "../data/articles"
+import CloudLogo from '../../icons/cloud-check-fill.svg'
 import DeleteBtn from "./DeleteBtn"
 import ListElement from "./ListElement"
 
@@ -7,10 +7,12 @@ export default function AppList(props) {
         <ul className="articles-list">
             {props.list.map(article => 
 
-            <li className="d-flex flex-column" key={article.id}>
-                <div className="article-head d-flex justify-content-between align-items-center">
-                    <ListElement title={article.title} id={article.id} AccF={props.AccF} current={props.current} category={article.category}/>
+            <li className="article-li d-flex flex-column" key={article.id}>
+                <div className="article-head d-flex justify-content-between align-items-center gap-4">
+                    <ListElement title={article.title} id={article.id} AccF={props.AccF} current={props.current} category={article.category} online={article.online}/>
+                    
                     <DeleteBtn id={article.id} handleF={props.handleF}/>
+
                 </div>
                 
                 {props.current === article.id && <p>{article.description}</p>}
--- src/assets/components/ListElement.jsx
+++ src/assets/components/ListElement.jsx
@@ -1,7 +1,15 @@
 export default function ListElement(props) {
     return(
         <div className={`article d-flex flex-column gap-4 ${props.category === 'UI/UX Design' ? 'UIUX' : props.category}`} key={props.id}>
-             <h8>{props.category}</h8>
+            
+            <div className="upper-title d-flex gap-2">
+                <h8>{props.category}</h8>
+                {props.online && 
+                    <svg className='online-badge' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+                    </svg>}
+            </div>
+            
             <h6 className={`head-text ${props.current == props.id ? 'active' : ''} `} onClick={props.AccF} data-id={props.id}> {props.title} </h6>
         </div>
     )
